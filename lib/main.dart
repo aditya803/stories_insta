e4f09b654b363161e9376bc9b8558cacdfc6fd85
Aditya Pandey <adityapan25@gmail.com>
2024-06-13 17:52:50 +0530

    added gesture detectors

--- lib/main.dart
+++ lib/main.dart
@@ -51,37 +51,69 @@ class _StoryScreenState extends State<StoryScreen> {
     _videoPlayerController.play();
   }
   Widget build(BuildContext context) {
+    final Story story = widget.stories[_currIndex];
     return Scaffold(
       backgroundColor: Colors.black,
-      body: PageView.builder(
-        controller: _pageController,
-        itemCount: widget.stories.length,
-        itemBuilder: (context,i){
-          final Story story = widget.stories[i];
-          switch(story.mediaType){
-            case MediaType.image:
-              return CachedNetworkImage(
-                  imageUrl: story.url,
-                  fit: BoxFit.cover,
-              );
-            case MediaType.video:
-              if(_videoPlayerController != null &&
-                 _videoPlayerController.value.isInitialized){
-                return FittedBox(
-                  fit: BoxFit.cover,
-                  child: SizedBox(
-                    width: _videoPlayerController.value.size.width,
-                    height: _videoPlayerController.value.size.height,
-                    child: VideoPlayer(_videoPlayerController),
-                  ),
+      body: GestureDetector(
+        onTapDown: (details) => _onTapDown(details, story),
+        child: PageView.builder(
+          controller: _pageController,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: widget.stories.length,
+          itemBuilder: (context,i){
+            final Story story = widget.stories[i];
+            switch(story.mediaType){
+              case MediaType.image:
+                return CachedNetworkImage(
+                    imageUrl: story.url,
+                    fit: BoxFit.cover,
                 );
-              }
-          }
-          return const SizedBox.shrink();
-        },
+              case MediaType.video:
+                if(_videoPlayerController != null &&
+                   _videoPlayerController.value.isInitialized){
+                  return FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: _videoPlayerController.value.size.width,
+                      height: _videoPlayerController.value.size.height,
+                      child: VideoPlayer(_videoPlayerController),
+                    ),
+                  );
+                }
+            }
+            return const SizedBox.shrink();
+          },
 
+        ),
       ),
     );
+  }
+  void _onTapDown(TapDownDetails details, Story story){
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double dx = details.globalPosition.dx;
+    if(dx < screenWidth/3){
+      setState(() {
+        if(_currIndex -1 >=0){
+          _currIndex-= 1;
+        }
+      });
+    }else if(dx>2*screenWidth / 3){
+      setState(() {
+        if(_currIndex + 1 < widget.stories.length){
+          _currIndex += 1;
+        }else{
+          _currIndex = 0;
+        }
+      });
+    }else{
+      if(story.mediaType == MediaType.video){
+        if(_videoPlayerController.value.isPlaying){
+          _videoPlayerController.pause();
+        }else{
+          _videoPlayerController.play();
+        }
+      }
+    }
   }
 }
 
